5a7c48e171758adfd8c489da484ebb487410afe0
Kibe Kevin <kibekevin215@gmail.com>
2025-10-22 22:37:40 +0300

    Add leaflet config file and import in main jsx file, for marker icons to render in production.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.scss"
+import "leaflet/dist/leaflet.css";
+import "./leaflet.config.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
