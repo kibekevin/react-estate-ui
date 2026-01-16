cf91386ea2ec96fe6975e234743ac98874470b87
Kibe Kevin <kibekevin215@gmail.com>
2026-01-16 07:05:02 +0300

    Update API base URL to production endpoint and add comment for local development

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: 'http://localhost:5500/api/v1',
+    baseURL: 'https://peaknestapi.onrender.com/api/v1',
     withCredentials: true
 })
 
 
 export default apiRequest;
+
+// http://localhost:5500/api/v1
