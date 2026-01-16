6c3a11e24887bf169b1ad194dc2cc9d7c73d5bf0
Kibe Kevin <kibekevin215@gmail.com>
2026-01-16 07:37:05 +0300

    Update API base URL to include versioning in endpoint

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: 'https://peaknestapi.onrender.com',
+    baseURL: 'https://peaknestapi.onrender.com/api/v1',
     withCredentials: true
 })
 
